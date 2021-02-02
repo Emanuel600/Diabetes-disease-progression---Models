dfd151c7fb56887b2476f2ccaee59d22e16b21da
Emanuel600 <78040463+Emanuel600@users.noreply.github.com>
2021-02-02 18:16:53 -0300

    Add files via upload

---
+++ f.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python3 
+# remeber to make file executable
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier as rfc
+from sklearn.metrics import ConfusionMatrixDisplay as cmd
+from sklearn.metrics import confusion_matrix as cm
+from sklearn.datasets import load_diabetes as ld
+from sklearn.model_selection import train_test_split as tts
+
+dd = ld()
+df = pd.DataFrame(dd['data'],columns=dd['feature_names'])
+df['target'] = dd['target']
+#Define x and y
+x = df.values
+y = df['target'].values
+t = []
+for i in range(len(y)):
+    if y[i]<50:
+        t.append('1')
+    elif y[i]<100:
+        t.append('2')
+    elif y[i]<150:
+        t.append('3')
+    elif y[i]<200:
+        t.append('4')
+    elif y[i]<250:
+        t.append('5')
+    elif y[i]<300:
+        t.append('6')
+    elif y[i]<350:
+        t.append('7')
+    else:
+        t.append('8')
+y = t
+#Split data
+xt,xtt,yt,ytt = tts(x,y,test_size=0.3)
+#Make model
+rm = rfc(n_estimators=20, max_depth=20, max_leaf_nodes=25, min_impurity_decrease=0.005)
+rm.fit(x,y)
+#Plot results
+l = cm(y,rm.predict(x))
+i = cmd(l)
+i.plot(cmap='Greens')
+plt.show()
